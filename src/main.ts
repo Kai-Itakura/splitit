c78044e84ce7947a820ad252e06b163b171c3141
k-itakura <k-itakura@ankh-systems.co.jp>
2025-02-18 08:14:23 +0000

    認証で使用するJWTに関する処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './interceptors/logging.intercepter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  // Logging Interceptor
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   // validation
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT ?? 3005);
-
-  app.enableCors();
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
---
+++ src/modules/auth/strategy/refresh.jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtRequestPayload } from './jwt.strategy';
+
+@Injectable()
+export class RefreshJwtStrategy extends PassportStrategy(
+  Strategy,
+  'refresh-jwt',
+) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const jwt: string | null = req.cookies['refresh_token'] ?? null;
+          console.log('🔥 ~ constructor ~ jwt:', jwt);
+          return jwt;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow('REFRESH_JWT_SECRET'),
+    });
+  }
+
+  validate(payload: JwtRequestPayload): JwtRequestPayload {
+    return payload;
+  }
+}
