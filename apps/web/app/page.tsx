008c62b1bea9c2a6a72f56b1400148b07e2396ce
k-itakura <k-itakura@ankh-systems.co.jp>
2025-04-17 18:52:31 +0900

    @hey-api/openapi-tsでリクエストヘッダーにCookieをセットすることができなかったので、openapi-typescriptにライブラリを変更

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,7 @@
-import { getAllGroups } from './api-client';
+import { client } from '@/openapi.config';
 
 export default async function Home() {
-  const result = await getAllGroups({ method: 'GET' });
-  console.log('🔥 ~ Home ~ result:', result);
+  const result = await client.POST('/auth/refresh');
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4"></div>
---
+++ apps/web/openapi.config.ts
@@ -0,0 +1,18 @@
+import createClient, { Middleware } from 'openapi-fetch';
+import { API_URL } from './app/constants/api-url';
+import { getCookieString } from './app/util/get-cookie-string';
+import { paths } from './openapi/schema';
+
+const openapiMiddleware: Middleware = {
+  async onRequest({ request }) {
+    request.headers.set('cookie', await getCookieString());
+  },
+
+  async onResponse({ response }) {
+    console.log('🔥 ~ onResponse ~ response:', response);
+  },
+};
+
+export const client = createClient<paths>({ baseUrl: API_URL });
+
+client.use(openapiMiddleware);
