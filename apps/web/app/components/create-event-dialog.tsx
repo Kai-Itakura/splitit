edc05547ef8febb81fbf650e4c1f5f7371c0f798
k-itakura <k-itakura@ankh-systems.co.jp>
2025-05-07 15:29:14 +0900

    イベント追加ボタンを実装

--- apps/web/app/components/create-event-dialog.tsx
+++ apps/web/app/components/create-event-dialog.tsx
@@ -1,26 +1,21 @@
 'use client';
 
 import {
-  Button,
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from '@repo/ui/components';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import CreateEventForm from './create-event-form';
 
-const CreateEventDialog = () => {
+const CreateEventDialog = ({ children }: { children: ReactNode }) => {
   const [open, setOpen] = useState(false);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="outline" className="flex mx-auto mt-6 cursor-pointer">
-          イベントを作成
-        </Button>
-      </DialogTrigger>
+      <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="sm:max-w-[425px] gap-8">
         <DialogHeader>
           <DialogTitle>イベント作成</DialogTitle>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,6 @@
 import { client } from '@/openapi.config';
+import { Button } from '@repo/ui/components';
+import { Plus } from '@repo/ui/components/icons';
 import Image from 'next/image';
 import CreateEventDialog from './components/create-event-dialog';
 import EventList from './components/event-list';
@@ -10,11 +12,18 @@ export default async function Home() {
     <div className="flex justify-center min-h-svh mt-10">
       <div className="w-full">
         <h1 className="font-extrabold text-center">イベント一覧</h1>
-        <ul className="mt-10">
+        <div className="mt-10">
           {data ? (
-            data.map((event) => (
-              <EventList event={event} key={event.id}></EventList>
-            ))
+            <>
+              <ul>
+                {data.map((event) => (
+                  <EventList event={event} key={event.id}></EventList>
+                ))}
+              </ul>
+              <CreateEventDialog>
+                <Plus className="fixed bottom-5 right-5 z-50 rounded-full shadow-xl p-1 w-10 h-10 bg-foreground hover:opacity-85 text-background cursor-pointer" />
+              </CreateEventDialog>
+            </>
           ) : (
             <>
               <Image
@@ -25,10 +34,17 @@ export default async function Home() {
                 className="shadow-2xl rounded-full w-64 h-64 object-contain mx-auto mb-8"
               />
               <p className="text-center text-lg">イベントがありません。</p>
-              <CreateEventDialog />
+              <CreateEventDialog>
+                <Button
+                  variant="outline"
+                  className="flex mx-auto mt-6 cursor-pointer"
+                >
+                  イベントを作成
+                </Button>
+              </CreateEventDialog>
             </>
           )}
-        </ul>
+        </div>
       </div>
     </div>
   );
