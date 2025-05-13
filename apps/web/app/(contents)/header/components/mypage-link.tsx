e2c8b5c0c9208d5023ffd976b7f78b87a052124d
k-itakura <k-itakura@ankh-systems.co.jp>
2025-05-13 13:20:02 +0900

    マイページへのリンクのスタイルを修正

--- apps/web/app/(contents)/header/components/mypage-link.tsx
+++ apps/web/app/(contents)/header/components/mypage-link.tsx
@@ -14,9 +14,11 @@ const MypageLink = ({
       <Link href={`/profile/${user.id}`} onClick={() => setOpen(false)}>
         <ProfileAvatar userId={user.id} size="lg" />
       </Link>
-      <div>
+      <div className="select-text">
         <p className="text-lg">{user.name}</p>
-        <p className="text-xs text-gray-500">ID: {user.id}</p>
+        <p className="text-xs text-gray-500">
+          ID: <span className="select-all">{user.id}</span>
+        </p>
       </div>
     </div>
   );
