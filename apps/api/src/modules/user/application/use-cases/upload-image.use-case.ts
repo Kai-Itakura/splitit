a36b85aea6d74875af63fdf82899c15e21e1cac9
Kai Itakura <itakai199969@gmail.com>
2025-09-28 00:27:04 +0900

    プロフィール画像を変更した時に古い画像が消えるように修正

--- apps/api/src/modules/user/application/use-cases/upload-image.use-case.ts
+++ apps/api/src/modules/user/application/use-cases/upload-image.use-case.ts
@@ -1,6 +1,10 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
+  IProfileImageStorage,
+  ProfileImageStorageToken,
+} from '../../domain/interfaces/profile-image.storage.interface';
+import {
   IUserRepository,
   UserRepositoryToken,
 } from '../../domain/interfaces/user.repository.interface';
@@ -10,12 +14,18 @@ export class UploadImageUseCase {
   constructor(
     @Inject(UserRepositoryToken)
     private readonly userRepository: IUserRepository,
+    @Inject(ProfileImageStorageToken)
+    private readonly profileImageStorage: IProfileImageStorage,
     private readonly configService: ConfigService,
   ) {}
 
   async execute(userId: string, image: Express.Multer.File) {
     const user = await this.userRepository.findById(userId);
     if (!user) throw new NotFoundException('User not found!');
+
+    if (user.imageFilepath) {
+      this.profileImageStorage.delete(user.imageFilepath);
+    }
 
     user.changeProfileImage(
       image.filename,
